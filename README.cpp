c1ec0b2f5ad9ed56d17480166d857f215608e860
Docik99 <32065929+Docik99@users.noreply.github.com>
2017-10-09 15:21:28 +0300

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -4,25 +4,39 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     string str;
     float a, c;
     char b;
     getline(cin, str);
     istringstream stream(str);
     stream >> c;
-    while (stream >> b) {
-        if (b == '+') {
+    while (stream >> b)
+    {
+        if (b == '+')
+        {
             stream >> a;
             c = c + a;
         }
-        else if (b == '-') {
+        else if (b == '-')
+        {
             stream >> a;
             c = c - a;
         }
+        else if (b == '*')
+        {
+            stream >> a;
+            c = c * a;
+        }
+        else if (b == '/')
+        {
+            stream >> a;
+            c = c / a;
+        }
+
     }
     cout << c;
-    cin.get();
     return 0;
 
 }
